3ad534ce7648e7a86ec306946c9c842a7d325e37
陈志辉 <chen_zhihui@cdv.com>
2018-05-31 13:25:56 +0800

    initial commit

---
+++ src/cpptmpl.cpp
@@ -0,0 +1,14 @@
+﻿// cpptmpl.cpp : Defines the entry point for the application.
+//
+
+#include "cpptmpl.h"
+#include <Poco/OSP/ServiceRegistry.h>
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	Poco::OSP::ServiceRegistry r;
+	return 0;
+}
---
+++ src/cpptmpl.h
@@ -0,0 +1,8 @@
+﻿// cpptmpl.h : Include file for standard system include files,
+// or project specific include files.
+
+#pragma once
+
+#include <iostream>
+
+// TODO: Reference additional headers your program requires here.
